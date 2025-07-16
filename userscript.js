1d20ba12d95dc34860bde7d54646ed28f7cb297c
HitsujiRere <55770125+HitsujiRere@users.noreply.github.com>
2025-07-16 23:04:10 +0900

    namespaceおよびversionを変更

--- userscript.js
+++ userscript.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name            Startpage.com Title with Query
-// @namespace       hitsujirere
-// @version         2025-07-16
+// @namespace       github.com/HitsujiRere/startpagecom-title-with-query
+// @version         1.0.0
 // @description     Append search query to title at Startpage.com
 // @description:ja  Startpage.comのタイトルに検索クエリを追加します
 // @author          HitsujiRere
 // @match           https://www.startpage.com/*
-// @icon            https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant           none
 // ==/UserScript==
 
